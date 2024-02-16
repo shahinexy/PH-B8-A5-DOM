af3e4bc8bc7fe17c8709ecae3c2ec7a58fffcda0
shahinexy <shahinexy@gmail.com>
2024-02-16 12:36:26 +0600

    total price section completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello');
+let count = 1;
+let totalPrice = 0;
+const cards = document.querySelectorAll('.card');
+for(const card of cards){
+    card.addEventListener('click', function(){
+    const cardTitle  =  card.querySelector('h3').innerText;
+    const taitelContainer = document.getElementById('title-container');
+    const p = document.createElement('p');
+    p.innerText = count + ' ' + cardTitle;
+    taitelContainer.appendChild(p)
+
+    const cardPrice = card.querySelector('span').innerText;
+    const price = parseFloat(cardPrice.split(' ')[1])
+    totalPrice += price;
+
+    document.getElementById('totalPrice').innerText = totalPrice;
+
+    count++
+    })
+}
